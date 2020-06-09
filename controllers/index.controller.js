82de7ac7a400e42e5b409afd0db71ffce4292824
LeandroDante LENOVO-DESKTOP <leandrodanteoliveira@hotmail.com>
2020-06-09 17:40:55 -0300

    Encapsulamento TopBar

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -177,68 +177,11 @@ app.controller("index.controller", function ($scope) {
     },
   };
 
-  $scope.topBarMood = [
-    {
-      displayName: "Positivo",
-      value: "1",
-    },
-    {
-      displayName: "Negativo",
-      value: "2",
-    },
-    {
-      displayName: "Neutro",
-      value: "3",
-    },
-    {
-      displayName: "Híbrido",
-      value: "4",
-    },
-    {
-      displayName: "Inválido",
-      value: "5",
-    },
-  ];
+  $scope.topBarMood = gettopBarMood();
 
-  $scope.topBarOrderBy = [
-    {
-      displayName: "Mais recentes",
-      value: "-dt_published",
-    },
-    {
-      displayName: "Mais antigos",
-      value: "dt_published",
-    },
-    {
-      displayName: "Mais amigos",
-      value: "",
-    },
-    {
-      displayName: "Mais seguidores",
-      value: "",
-    },
-    {
-      displayName: "Mais curtidas",
-      value: "-do_liked",
-    },
-    {
-      displayName: "Mais compartilhamentos",
-      value: "",
-    },
-    {
-      displayName: "Mais comentários",
-      value: "",
-    },
-  ];
+  $scope.topBarOrderBy = gettopBarOrderBy();
 
-  $scope.topBarRefresh = [
-    {
-      displayName: "Manual",
-    },
-    {
-      displayName: "Automático",
-    },
-  ];
+  $scope.topBarRefresh = gettopBarRefresh();
 
   $scope.orderOption = {
     value: "id_smpost",
